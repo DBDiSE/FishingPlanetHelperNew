fcf5654bba2438248a3d7550f48b305d169a0fad
Piotr Mazurkiewicz <piotr.mazurkiewicz@hotmail.com>
2018-06-21 00:29:34 +0200

    Initial

---
+++ FishingPlanetHelper/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace FishingPlanetHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(520, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Path to FishingPlanet game:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(350, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse.";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(519, 82);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(9, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(445, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Inject!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 170);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ FishingPlanetHelper/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FishingPlanetHelper
+{
+    public partial class Form1 : Form
+    {
+        string GamePath = @"C:\Program Files(x86)\Steam\steamapps\common\Fishing Planet\FishingPlanet_Data\Managed";
+        string DllPath = @"C:\Program Files(x86)\Steam\steamapps\common\Fishing Planet\FishingPlanet_Data\Managed\Assembly-CSharp.dll";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox1.Text = GamePath;
+
+            var versInfo = FileVersionInfo.GetVersionInfo("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Fishing Planet\\FishingPlanet_Data\\Managed\\Assembly-CSharp.dll");
+            String fileVersion = versInfo.FileVersion;
+            label2.Text = "Csharp.dll FOUND = " + fileVersion;
+        }
+
+        public void CheckDll()
+        {
+            string Path;
+
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Path = folderBrowserDialog1.SelectedPath + "\\FishingPlanet_Data\\Managed";
+                textBox1.Text = Path;
+
+                string[] file = Directory.GetFiles(Path, "Assembly-CSharp.dll");
+
+                if (file.Length != 0)
+                {
+                    label2.Text = "File found!";
+                }
+                else
+                {
+                    label2.Text = "File not found!";
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CheckDll();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Starting");
+
+            var versInfo = FileVersionInfo.GetVersionInfo("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Fishing Planet\\FishingPlanet_Data\\Managed\\Assembly-CSharp.dll");
+            String fileVersion = versInfo.FileVersion;
+            listBox1.Items.Add("CSharp.dll version = " + fileVersion);
+            label2.Text = "Csharp.dll FOUND - current version = " + fileVersion;
+
+            System.Threading.Thread.Sleep(200);
+            listBox1.Items.Add("Backup original file");
+            if (File.Exists("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Fishing Planet\\FishingPlanet_Data\\Managed\\Assembly-CSharp.dll.bak"))
+            {
+                File.Delete("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Fishing Planet\\FishingPlanet_Data\\Managed\\Assembly-CSharp.dll.bak");
+            }
+            System.IO.File.Move("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Fishing Planet\\FishingPlanet_Data\\Managed\\Assembly-CSharp.dll", "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Fishing Planet\\FishingPlanet_Data\\Managed\\Assembly-CSharp.dll.bak");
+
+            System.Threading.Thread.Sleep(200);
+            listBox1.Items.Add("Copying files..");
+            
+
+            //string[] files = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceNames();
+            //foreach(string element in files)
+            //{
+            //    listBox1.Items.Add(element);
+            //}
+
+            var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("FishingPlanetHelper.Assembly-CSharp.dll");
+            var fileStream = File.Create("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Fishing Planet\\FishingPlanet_Data\\Managed\\Assembly-CSharp.dll");
+            stream.Seek(0, SeekOrigin.Begin);
+            stream.CopyTo(fileStream);
+            fileStream.Close();
+
+            System.Threading.Thread.Sleep(200);
+            listBox1.Items.Add("Done!");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
